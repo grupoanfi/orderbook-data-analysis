de93c20ba9cde660a4380fde7ba9227d48c725fa
Juan Camilo David <juanqui12@gmail.com>
2016-03-28 13:18:47 -0500

    initializing setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='ODA',
+    version='1.0.0',
+    description='OrderBook utilities',
+    author='ANFI',
+    author_email='grupoanfi@gmail.com',
+    packages=['ODA', 'ODA.test'],
+    license='OSI Approved',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Legal Industry',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Office/Business :: Financial',
+    ]
+)
+
