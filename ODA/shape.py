1df44ab76d73e1ab411d934206f0e0c41657aaf1
Luis Bermudez <luisbermudez.grupoanfi@gmail.com>
2016-03-31 18:45:16 -0500

    modificado shape

--- ODA/shape.py
+++ ODA/shape.py
@@ -15,50 +15,29 @@ from scipy.stats import gaussian_kde
 
 mpl.rc("figure", facecolor="white")
 
-def generate_domain_line(points_ask, points_bid):
-    """
-    :param points_ask:
-    :param points_bid:
-    :return:
-    """
-
-    # mid_price = (points_ask[0][0] + points_bid[0][0]) / 2.0
-    mid_size = (points_ask[0][1] + points_bid[0][1]) / 2.0
-
-    # all_points_x = [point[0] for point in points_bid]
-    # all_points_x.append(mid_price)
-    # all_points_x.extend([point[0] for point in points_ask])
-    # all_points_x.sort()
-
-    all_points_y = [point[1] for point in points_bid]
-    all_points_y.reverse()
-    all_points_y.append(mid_size)
-    all_points_y.extend([point[1] for point in points_ask])
-
-    return all_points_y
-
-
-def gaussian_kenel():
-    def f(x):
-        return 1.0/np.sqrt(2*np.pi)*np.exp(-1.0/2*x**2)
-
-    return f
-
 
 class kernelhandler(object):
-    def __init__(self, x, bandwidth):
+    def __init__(self, points, bandwidth):
         self.bandwidth = bandwidth
-        self.x = x
-        self.l = None
+        self.points = points
+
+
+    def kernel(self):
+        def f(x):
+            return 1.0/np.sqrt(2*np.pi)*np.exp(-1.0/2*x**2)
+
+        return f
 
     def __call__(self, *args, **kwargs):
         x = kwargs['x']
-        if kwargs['kernel'] == 'gauss':
-            print 'entro'
-            f = gaussian_kenel()
-            self.l = [f((x - x_i)*1.0/self.bandwidth) for x_i in self.x]
-            print self.l, [(x - x_i)*1.0/self.bandwidth for x_i in self.x]
-        return (1.0/len(self.x)*self.bandwidth)*sum(self.l)
+
+        f = self.kernel()
+
+        inter = [f((x - x_i)*1.0/self.bandwidth)*y_i for x_i, y_i in self.points]
+
+        weigth = [f((x - x_i)*1.0/self.bandwidth) for x_i, y_i in self.points]
+
+        return ((1.0/sum(weigth))*sum(inter))
 
 
 
@@ -71,6 +50,9 @@ class Shape(object):
         :return:
         """
         self.data = data
+        self.size_k = size_k
+
+
         self.__fig = None
         self.__ax = None
         self.__line_bid = None
@@ -78,26 +60,18 @@ class Shape(object):
         self.__scat = None
         self.__patch_ask = None
         self.__patch_bid = None
+        self.anot = None
         self.ticks = 500
-        self.size_k = size_k
+
         self.__vert_ask = None
         self.__codes = None
         self.__vert_bid = None
         self.__cargando_figuras()
 
-    def __domain_normalizade(self, p_bid, p_ask):
-        """
-        :param p_bid:
-        :param p_ask:
-        :return:
-        """
-        return range(-len(p_bid), len(p_ask) + 1)
-
     def __bid_normalizade(self, p_bid):
         return range(-len(p_bid), 0)
 
-    @staticmethod
-    def __ask_normalizade(p_ask):
+    def __ask_normalizade(self, p_ask):
         return range(1, len(p_ask) + 1)
 
     def __cargando_figuras(self):
@@ -105,8 +79,8 @@ class Shape(object):
         :return:
         """
         self.__fig, self.__ax = plt.subplots()
-        self.__line_bid, = self.__ax.plot([], [], lw=1, c='#286090')
-        self.__line_ask, = self.__ax.plot([], [], lw=1, c='#286090')
+        self.__line_bid, = self.__ax.plot([], [], lw=2, c='#286090')
+        self.__line_ask, = self.__ax.plot([], [], lw=2, c='#286090')
         self.__scat = self.__ax.scatter([], [], c='black', s=2)
 
         self.__ax.set_ylim(-1, 30)
@@ -117,7 +91,8 @@ class Shape(object):
         self.__vert_ask = [f(x) for x in xrange(self.size_k + 1)]
         self.__vert_ask = np.array(sum(self.__vert_ask, []))
 
-        self.__vert_bid = [[(float(x) - 0.5, 0.), (float(x) - 0.5, 0.)] for x in xrange(-(self.size_k), 1)]
+        f = lambda x: [(float(x) - 0.5, 0.), (float(x) - 0.5, 0.)]
+        self.__vert_bid = [f(x) for x in xrange(-(self.size_k), 1)]
         self.__vert_bid = np.array(sum(self.__vert_bid, []))
 
         self.__codes = [path.Path.LINETO for _ in xrange(len(self.__vert_ask))]
@@ -146,6 +121,9 @@ class Shape(object):
         self.__ax.tick_params(axis='x', colors='#404040')
         self.__ax.tick_params(axis='y', colors='#404040')
 
+        self.anot =  self.__ax.text(0.02, 0.95, '', transform=self.__ax.transAxes)
+
+
     def __update_limits_x(self, domain):
         """
         :param domain:
@@ -161,16 +139,16 @@ class Shape(object):
         if xmax - 2 < max_domain:
             xmax = max_domain + 4
             any_change = True
-        else:
-            if abs(xmax - max_domain) > 10:
+
+        elif abs(xmax - max_domain) > 10:
                 xmax = max_domain + 3
                 any_change = True
 
         if xmin + 2 > min_domain :
             xmin = min_domain - 2
             any_change = True
-        else:
-            if abs(xmin - min_domain) > 10:
+
+        elif abs(xmin - min_domain) > 10:
                 xmin = min_domain - 4
                 any_change = True
 
@@ -195,16 +173,16 @@ class Shape(object):
         if ymax  < max_rango:
             ymax = max_rango + 2
             any_change = True
-        else:
-            if abs(ymax - max_rango) > 10:
+
+        elif abs(ymax - max_rango) > 10:
                 ymax = max_rango + 3
                 any_change = True
 
         if ymin > min_rango :
             ymin = min_rango - 2
             any_change = True
-        else:
-            if abs(ymin - min_rango) > 10:
+
+        elif abs(ymin - min_rango) > 10:
                 ymin = min_rango - 3
                 any_change = True
 
@@ -219,13 +197,10 @@ class Shape(object):
         :param y:
         :return:
         """
-        f = PchipInterpolator(x,y, extrapolate=True)
-        # f = interp1d(x, y, kind=3)
-        k = kernelhandler(y,10)
-        # tck = splrep(x,y, s=20, k=3)
+        f = PchipInterpolator(x , y , extrapolate=True)
+
         domain = np.linspace(x[0], x[-1], num=self.ticks, endpoint=True)
-        # y2 = splev(domain, tck)
-        # return y2
+
         return [f(a) for a in domain]
 
     def __generator_data(self):
@@ -236,25 +211,17 @@ class Shape(object):
 
         for orderbook in self.data:
 
-            x_ask = [data[1] for data in orderbook[-1]]
-            x_bid = [data[1] for data in orderbook[1]]
 
             y_ask = [data[1] for data in orderbook[-1]]
             y_bid = [data[1] for data in orderbook[1]]
 
-            # y_bid.reverse()
+            x_ask = self.__ask_normalizade(y_ask)
+            x_bid = self.__bid_normalizade(y_bid)
 
-            x_ask_norm = self.__ask_normalizade(x_ask)
-            x_bid_norm = self.__bid_normalizade(x_bid)
 
-            # all_poinst_y = generate_domain_line(orderbook[-1], orderbook[1])
-            # all_poinst_x = self.__domain_normalizade(p_bid, p_ask)
+            yield self.__interpolate_data(x_ask, y_ask), self.__interpolate_data(x_bid, y_bid), x_ask, x_bid, y_ask, y_bid
 
-            # assert all_poinst_y[-len(p_ask):] == p_ask
 
-            yield self.__interpolate_data(x_ask_norm, y_ask), self.__interpolate_data(x_bid_norm, y_bid), x_ask_norm, x_bid_norm, y_ask, y_bid
-
-            # yield self.__interpolate_data(all_poinst_x, all_poinst_y), all_poinst_x, all_poinst_y, p_ask, p_bid
 
     def __run_data(self, data):
         """
@@ -262,14 +229,22 @@ class Shape(object):
         :param data:
         :return:
         """
+
+
         ask_inter_points, bid_inter_points, x_ask, x_bid, y_ask, y_bid = data
+
+        mid_price = (y_ask[0]  + y_bid[0])*1.0 / 2
+
+        # print mid_price
+
+        self.anot.set_text('Mid Price = %.1f' % mid_price)
 
         bid_inter_points.reverse()
 
         self.__update_limits_x(x_ask + x_bid)
         self.__update_limits_y(ask_inter_points + bid_inter_points)
 
-        self.__line_bid.set_data(np.linspace(x_bid[0], x_bid[-1], num=self.ticks, endpoint=True), bid_inter_points)
+        self.__line_bid.set_data(np.linspace(x_bid[0], x_bid[-1], num=self.ticks, endpoint=True), bid_inter_points,)
         self.__line_ask.set_data(np.linspace(x_ask[0], x_ask[-1], num=self.ticks, endpoint=True), ask_inter_points)
 
         bid_copy = y_bid[:]
@@ -294,17 +269,13 @@ class Shape(object):
             self.__vert_bid[a-(x*2+1)][1] = 0
             self.__vert_bid[a-(x*2+2)][1] = 0
 
-        return [self.__patch_ask, self.__patch_bid,self.__line_bid, self.__line_ask,self.__scat,]
+        return [self.__patch_ask, self.__patch_bid,self.__line_bid, self.__line_ask, self.__scat, self.anot]
+
 
     def __call__(self, *args, **kwargs):
-        ani = animation.FuncAnimation(self.__fig, self.__run_data, self.__generator_data, blit=True, interval=100, repeat=False)
+        ani = animation.FuncAnimation(self.__fig, self.__run_data, self.__generator_data, blit=True, interval=30, repeat=False)
         plt.show()
 
-
-class ShapeNornal(Shape):
-    def __init__(self, data, size_k, normal):
-        super(ShapeNornal, self). __init__(data, size_k)
-        self.normal = normal
 
 
 def generar_orderbook_lines(n_lines):
@@ -313,6 +284,7 @@ def generar_orderbook_lines(n_lines):
     for i in xrange(n_lines):
         n_ask = random.randint(3, 6)
         ask = [(random.random() * 9 + 10, random.random() * 5 + 1) for i in xrange(n_ask)]
+        ask.sort(key = lambda x: x[0])
 
         n_bid = random.randint(3, 6)
         bid = [(random.random() * 8 + 1, random.random() * 5 + 1) for i in xrange(n_bid)]
@@ -325,3 +297,19 @@ if __name__ == '__main__':
     lines = generar_orderbook_lines(500)
     myshape = Shape(lines, 10)
     myshape()
+
+    l = [(i, random.random()) for i in xrange(10)]
+    k = kernelhandler(l, 0.5)
+    s = [k(**{'x': x}) for x, y in l]
+
+    print [p[1] for p in l]
+    print s
+
+    plt.plot([p[0] for p in l], [p[1] for p in l] )
+
+    domain = np.linspace(0, 10, 100)
+
+    plt.plot(domain, [k(**{'x': x}) for x in domain])
+
+    plt.show()
+
