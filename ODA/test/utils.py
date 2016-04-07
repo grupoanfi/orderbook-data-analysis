7261158291a479e0a791b3548a2c301809930e69
Juan Camilo David <juanqui12@gmail.com>
2016-04-07 17:15:36 -0500

    writing more utilities

--- ODA/test/utils.py
+++ ODA/test/utils.py
@@ -6,8 +6,8 @@ import time
 from collections import OrderedDict
 import random as rd
 import threading
-from ODA.market_cols import OrderKeys
-
+from ODA.market import Request
+from ODA.market_cols import OrderKeys, LobsterKeys
 
 class actual_time(threading.Thread):
     def __init__(self, time):
@@ -114,6 +114,56 @@ class Generear_ordenes_limit():
             cont += 1
 
 
+def get_meta_limit(price=0, size=0, id_user='juan', group='Bancolombia',
+                   id_order=1):
+    req = {
+        OrderKeys.size: size,
+        OrderKeys.direction: None,
+        OrderKeys.price: price,
+        OrderKeys.id_user: id_user,
+        OrderKeys.group: group,
+        OrderKeys.event: LobsterKeys.add_limit_order,
+        OrderKeys.id_order: id_order
+    }
+    return req
+
+
+def gen_request_limit_bid(price=0, size=0, id_user='default', group='Bancolombia',
+                          id_order=1):
+    req = get_meta_limit(price, size, id_user, group, id_order)
+    req[OrderKeys.direction] = LobsterKeys.bid
+    return req
+
+
+def gen_request_limit_ask(price=0, size=0, id_user='default', group='Bancolombia',
+                          id_order=1):
+    req = get_meta_limit(price, size, id_user, group, id_order)
+    req[OrderKeys.direction] = LobsterKeys.ask
+    return req
+
+
+def gen_random_limit_orders_around(num, midprice=500, unique_ids=False):
+
+    if unique_ids:
+        id_list = rd.sample(xrange(1, 2*num), num)
+    else:
+        id_list = [1]*num
+
+    list_orders = list()
+    for i in xrange(num):
+        direction = rd.choice(LobsterKeys.directions)
+        size = rd.randint(10, 1000)
+        id_order = id_list[i]
+
+        if direction == LobsterKeys.bid:
+            price = rd.randint(1, midprice-1)
+            order = gen_request_limit_bid(price, size, id_order=id_order)
+        else:
+            price = rd.randint(midprice+1, 10**5)
+            order = gen_request_limit_ask(price, size, id_order=id_order)
+
+        list_orders.append(order)
+    return list_orders
 
 
 
@@ -124,15 +174,3 @@ def tex_alearotio(id, target):
     d = chr(rd.randint(97, 122))
 
     return str(id) + '-' + str(target) + '-' + a + b + c + d
-
-
-
-
-
-
-
-
-
-
-
-
